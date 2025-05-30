4519b4bf3ceb64bd5274d096cf0298c56ba3bebc
MarkEKraus <git@markekraus.com>
2025-05-30 14:07:04 -0700

    Add remaining property types

--- src/TableBuilderExtensionsUnityEngine.cs
+++ src/TableBuilderExtensionsUnityEngine.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 
 namespace PlayMakerDocumenter;
@@ -28,51 +29,71 @@ internal static class TableBuilderExtensionsUnityEngine
             .AddRow($"{Property}.{nameof(Value.isDone)}", Value.isDone)
             .AddRow($"{Property}.{nameof(Value.priority)}", Value.priority)
             .AddRow($"{Property}.{nameof(Value.progress)}", Value.progress);
-    
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, AudioClip Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.ambisonic)}", Value.ambisonic)
+            .AddRow($"{Property}.{nameof(Value.channels)}", Value.channels)
+            .AddRow($"{Property}.{nameof(Value.frequency)}", Value.frequency)
+            .AddRow($"{Property}.{nameof(Value.isReadyToPlay)}", Value.isReadyToPlay)
+            .AddRow($"{Property}.{nameof(Value.length)}", Value.length)
+            .AddRow($"{Property}.{nameof(Value.loadInBackground)}", Value.loadInBackground)
+            .AddRow($"{Property}.{nameof(Value.loadState)}", Value.loadState)
+            .AddRow($"{Property}.{nameof(Value.loadType)}", Value.loadType)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name)
+            .AddRow($"{Property}.{nameof(Value.preloadAudioData)}", Value.preloadAudioData)
+            .AddRow($"{Property}.{nameof(Value.samples)}", Value.samples);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, AudioSource Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.enabled)}", Value.enabled)
+            .AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, Camera Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.enabled)}", Value.enabled)
+            .AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, KeyCode Value, ActionContext ctx = null) =>
+        tb.AddRow(Property, Value.ToString());
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, Light Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.enabled)}", Value.enabled)
+            .AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
     public static TableBuilder AddRow(this TableBuilder tb, string Property, ParticleSystem Value, ActionContext ctx = null) =>
         Value is null
         ? tb
-        : tb.AddRow($"{Property}.{nameof(Value.automaticCullingEnabled)}", Value.automaticCullingEnabled)
-            .AddRow($"{Property}.{nameof(Value.duration)}", Value.duration)
-            .AddRow($"{Property}.{nameof(Value.emissionRate)}", Value.emissionRate)
-            .AddRow($"{Property}.{nameof(Value.enableEmission)}", Value.enableEmission)
+        : tb.AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, Renderer Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.enabled)}", Value.enabled)
             .AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
-            .AddRow($"{Property}.{nameof(Value.gravityModifier)}", Value.gravityModifier)
-            .AddRow($"{Property}.{nameof(Value.isEmitting)}", Value.isEmitting)
-            .AddRow($"{Property}.{nameof(Value.isPaused)}", Value.isPaused)
-            .AddRow($"{Property}.{nameof(Value.isPlaying)}", Value.isPlaying)
-            .AddRow($"{Property}.{nameof(Value.isStopped)}", Value.isStopped)
-            .AddRow($"{Property}.{nameof(Value.loop)}", Value.loop)
-            .AddRow($"{Property}.{nameof(Value.maxParticles)}", Value.maxParticles)
-            .AddRow($"{Property}.{nameof(Value.name)}", Value.name)
-            .AddRow($"{Property}.{nameof(Value.particleCount)}", Value.particleCount)
-            .AddRow($"{Property}.{nameof(Value.playbackSpeed)}", Value.playbackSpeed)
-            .AddRow($"{Property}.{nameof(Value.playOnAwake)}", Value.playOnAwake)
-            .AddRow($"{Property}.{nameof(Value.proceduralSimulationSupported)}", Value.proceduralSimulationSupported)
-            .AddRow($"{Property}.{nameof(Value.randomSeed)}", Value.randomSeed)
-            .AddRow($"{Property}.{nameof(Value.startColor)}", Value.startColor)
-            .AddRow($"{Property}.{nameof(Value.startLifetime)}", Value.startLifetime)
-            .AddRow($"{Property}.{nameof(Value.startRotation)}", Value.startRotation)
-            .AddRow($"{Property}.{nameof(Value.startRotation3D)}", Value.startRotation3D)
-            .AddRow($"{Property}.{nameof(Value.startSize)}", Value.startSize)
-            .AddRow($"{Property}.{nameof(Value.startSpeed)}", Value.startSpeed)
-            .AddRow($"{Property}.{nameof(Value.time)}", Value.time)
-            .AddRow($"{Property}.{nameof(Value.useAutoRandomSeed)}", Value.useAutoRandomSeed);
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, Rigidbody Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, Rigidbody2D Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
+
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, VideoClip Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.name)}", Value.name)
+            .AddRow($"{Property}.{nameof(Value.originalPath)}", Value.originalPath);
+    public static TableBuilder AddRow(this TableBuilder tb, string Property, VideoPlayer Value, ActionContext ctx = null) =>
+        Value is null
+        ? tb
+        : tb.AddRow($"{Property}.{nameof(Value.enabled)}", Value.enabled)
+            .AddRow($"{Property}.{nameof(Value.gameObject)}", Value.gameObject)
+            .AddRow($"{Property}.{nameof(Value.name)}", Value.name);
 }
-
-
-/*
-UnityEngine.AudioClip
-UnityEngine.AudioSource
-UnityEngine.Camera
-UnityEngine.Color
-UnityEngine.KeyCode
-UnityEngine.Light
-UnityEngine.Renderer
-UnityEngine.Rigidbody
-UnityEngine.Rigidbody2D
-UnityEngine.Space
-UnityEngine.Vector3
-UnityEngine.Video.VideoClip
-UnityEngine.Video.VideoPlayer
-*/
