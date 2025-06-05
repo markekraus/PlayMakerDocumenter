6a64e4723401f22a96b3ac78944c594e075e490e
MarkEKraus <git@markekraus.com>
2025-06-04 17:37:59 -0700

    fixy a whoopsie

--- PlayMakerDocumenter.Serializer/FsmActionProperty.cs
+++ PlayMakerDocumenter.Serializer/FsmActionProperty.cs
@@ -5,6 +5,8 @@ public record FsmActionProperty
     public string Property;
     public string Value;
     public FsmActionProperty() {}
+    public FsmActionProperty(string Property, string Value) =>
+        (this.Property, this.Value) = (Property, Value);
     public FsmActionProperty(FsmVariableDoc fsmVar) =>
         (Property, Value) = (fsmVar.Name, fsmVar.Value);
     public static implicit operator FsmActionProperty(FsmVariableDoc fsmVar) =>
